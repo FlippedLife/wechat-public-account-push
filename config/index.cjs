1c71374f2ad18042ac9ee94458d5efe38f48963f
FlippedLife <111755181+FlippedLife@users.noreply.github.com>
2024-07-30 13:44:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oKjm_6olLpBuLHz3b9uTh3IcUmqs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'amqcF4qW-oGaEd9eKTnZ9r-tnK2JBEK9VujYUJdE66g',
+      useTemplateId: 'U6AGm_s7OgBSI8Pq7dKL-nP8uPobwF6lQL3kC3skCOU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-12',
       horoscopeDateType:"今日",
@@ -61,7 +61,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oKjm_6nrYcvEy6PEHgnv1Jp85Ih4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'amqcF4qW-oGaEd9eKTnZ9r-tnK2JBEK9VujYUJdE66g',
+      useTemplateId: 'U6AGm_s7OgBSI8Pq7dKL-nP8uPobwF6lQL3kC3skCOU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-01',
       horoscopeDateType:"今日",
