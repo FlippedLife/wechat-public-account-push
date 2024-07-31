e090d53a779fbf55090556220e582f2e3db4dc09
FlippedLife <111755181+FlippedLife@users.noreply.github.com>
2024-07-31 09:20:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'f2c2906c7894e4f6843b53b77347041c',
 
-  // PROVINCE: '上海',
-  CITY: '上海',
+  PROVINCE: '上海市',
+  CITY: '上海市',
 
   USERS: [
     {
